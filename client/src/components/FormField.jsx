aa7ed2380796211771c45de32249173f3c8ed7f2
koushith <koushith97@gmail.com>
2023-11-07 12:03:26 +0530

    adds create campaign form

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const FormField = ({ labelName, placeholder, inputType, isTextArea, value, handleChange }) => {
+export const FormField = ({ labelName, placeholder, inputType, isTextArea, value, handleChange }) => {
   return (
     <label className="flex-1 w-full flex flex-col">
       {labelName && (
@@ -30,4 +30,3 @@ const FormField = ({ labelName, placeholder, inputType, isTextArea, value, handl
   )
 }
 
-export default FormField
